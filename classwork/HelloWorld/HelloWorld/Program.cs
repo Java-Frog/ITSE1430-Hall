5d27f6bbe918a50b52d7837b59d63af53f35c1b0
MHall <michael.hall798@My.tccd.edu>
2023-08-24 21:35:54 -0500

    Initial Version

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Michael A Hall
+ITSE 1430
+Fall 2023
+*/
+class Program
+{ 
+    static void Main()
+    {
+       
+        //Console.WriteLine("Hello " + "World");
+        //Console.WriteLine(10 + 15);
+        //Console.WriteLine('A');
+        //Console.WriteLine(3.14);
+ 
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello World " + name);
+    }
+
+}
