37648b7df59e02f2ab567c1821eb4d7b286b2b74
MHall <michael.hall798@My.tccd.edu>
2023-08-29 21:16:48 -0500

    Start of MovieLibrary and Basic Setup for Lab1

---
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,29 @@
+﻿//Movie library to catalog a movie collection
+//TODO: Get Movie Data
+//--title, genre, descrip, mpaa rating
+//--length, release year, budget
+//--IsBlackAndWhite
+//TODO: Operations Add, edit, view, delete
+
+
+string title = "", genre = "", description = "", rating = "", input;
+int length = 0, releaseYear = 1900;
+decimal budget=125.45M;
+bool isBlackAndWhite=false;
+
+Console.WriteLine("Enter a title: ");
+input = Console.ReadLine();
+title = input;
+
+Console.WriteLine("Enter a description: ");
+input = Console.ReadLine();
+description = input;
+
+//TODO: Convert to into
+//Console.WriteLine("Enter the run length (in minutes): ");
+//input = Console.ReadLine();
+//title = input;
+
+Console.WriteLine(title);
+Console.WriteLine(description);
+Console.WriteLine(length);
---
+++ labs/Lab1/MichaelHall.AdventureGame.ConsoleHost/MichaelHall.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Michael A. Hall
+ * ITSE 1234
+ * Fall 2023
+ */
+
+//TODO make player class
+//TODO make room class
+//TODO make random event trigger
