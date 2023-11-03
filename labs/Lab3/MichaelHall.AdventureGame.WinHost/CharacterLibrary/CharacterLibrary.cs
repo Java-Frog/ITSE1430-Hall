1f02e1d27ed9b9cd10ce79dab83d06894c182e12
Michael Hall <142953747+Java-Frog@users.noreply.github.com>
2023-11-03 13:06:14 -0500

    Add files via upload

---
+++ labs/Lab3/MichaelHall.AdventureGame.WinHost/CharacterLibrary/CharacterLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using MichaelHall.AdventureGame;
+
+namespace CharacterDatabase
+{
+    public class MemoryCharacterDatabase
+    {
+        private List<Character> characters = new List<Character>();
+
+        public void AddCharacter(Character character)
+        {
+            // Add the character to your database
+            characters.Add(character);
+        }
+
+        public void RemoveCharacter(Character character)
+        {
+            // Remove the character from your database
+            characters.Remove(character);
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            // Return a list of all characters in your database
+            return characters;
+        }
+
+        public Character GetCharacterByName(string name)
+        {
+            // Find and return a character by name
+            return characters.Find(character => character.Name == name);
+        }
+    }
+}
