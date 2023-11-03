3553eebadb15b3df68c6c726cf4d200876ac8166
Michael Hall <142953747+Java-Frog@users.noreply.github.com>
2023-11-03 13:08:44 -0500

    Add files via upload

---
+++ labs/Lab3/MichaelHall.AdventureGame.WinHost/MichaelHall.AdventureGame/ValidateableObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelHall.AdventureGame
+{
+    public class ValidateableObject
+    {
+        public bool IsValid
+        {
+            get { return TryValidate(out var _); }
+        }
+
+        public virtual bool TryValidate( out string message)
+        {
+            message = "";
+            return true;
+        }
+
+        public string Validate()
+        {
+            if(!TryValidate(out var message))
+            {
+                return message;
+            }
+            return "";
+        }
+
+    }
+}
